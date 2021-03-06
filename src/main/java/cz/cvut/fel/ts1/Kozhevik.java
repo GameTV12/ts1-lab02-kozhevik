5829727cf31dade19456edad1f8adbd36e3825b6
GameTVCity <kozhemyakinvic@mail.ru>
2021-03-06 20:48:29 +0100

    -

--- src/main/java/cz/cvut/fel/ts1/Kozhevik.java
+++ src/main/java/cz/cvut/fel/ts1/Kozhevik.java
@@ -2,10 +2,12 @@ package cz.cvut.fel.ts1;
 
 public class Kozhevik {
     public long factorial (int n){
-        int result = n;
-        for (int i = 1; i<n; i++){
-            result = result * (n - i);
+        int result=1,i=1;
+        while(i<=n){
+            result=result*i;
+            i++;
         }
+
         return result;
     }
 }
